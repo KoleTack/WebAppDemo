b77c56a83ff5c567f8fdb6095409e3235db8cc5c
Kole Tackney <Kole.tackney@gmail.com>
2018-06-04 18:00:56 -0400

    Upload to github
    original code

---
+++ WebApplication1/Pages/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1.Pages
+{
+    public class CreateModel : PageModel
+    {
+        private readonly AppDbContext _db;
+
+        private ILogger<CreateModel> m_Log;
+
+
+        public CreateModel(AppDbContext db, ILogger<CreateModel> log)
+        {
+            _db = db;
+            m_Log = log;
+        }
+
+        [TempData]
+        public string Msg { get; set; }
+
+        [BindProperty]
+        public Customer Customer { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.Customers.Add(Customer);
+            await _db.SaveChangesAsync();
+            Msg = $"Customer {Customer.Name} Added!";
+            m_Log.LogCritical(Msg);
+            return RedirectToPage("/Index");
+        }
+
+    }
+}
---
+++ WebApplication1/Pages/Edit.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Pages
+{
+    public class EditModel : PageModel
+    {
+        private readonly AppDbContext _db;
+
+        public EditModel(AppDbContext db) { _db = db; }
+
+        [TempData]
+        public string Msg { get; set; }
+        
+        [BindProperty]
+        public Customer customer { get; set; }
+
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            customer = await _db.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                //Adress is missing
+                return RedirectToPage("/Index");
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.Attach(customer).State = EntityState.Modified;
+            Msg = $"Customer {customer.Name} has been updated!";
+
+            try
+            {
+                await _db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException E)
+            {
+                throw new Exception($"Customer {customer.ID} not found!", E);
+            }
+
+            return RedirectToPage("./Index");
+
+        }
+
+
+
+    }
+}
